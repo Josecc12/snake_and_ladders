2a9d640dc80a5a42a6073dfc843925a87013b1ec
Jose Daniel Chocoy <danielchocoycordon@hotmail.es>
2021-02-12 22:52:48 -0600

    Add: Class Box & Player

--- Proyecto1/src/main/java/com/mycompany/proyecto1/GameForm.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/GameForm.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.proyecto1;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author Daniel
@@ -14,12 +17,38 @@ public class GameForm extends javax.swing.JFrame {
     /**
      * Creates new form GameForm
      */
+    int contador;
+    Box casillas[]=new Box[25];
+    Player jugador=new Player();
     public GameForm() {
         initComponents();
+        
         this.setLocationRelativeTo(null);
         MenuForm menu=new MenuForm();
-        jLabel1.setText(String.valueOf(menu.getPlayers()));
+        contador=0;
+        for (int i = 0; i < 6; i++) {
+            casillas[i]=new Box(50, 450);
+        }
+        for (int i = 6; i < 12; i++) {
+            casillas[i]=new Box(0, 350);
+        }
+        for (int i = 12; i < 25; i++) {
+            casillas[i]=new Box(50, 280);
+        }
         
+        casillas[1].setX(150);
+        casillas[2].setX(220);
+        casillas[3].setX(320);
+        casillas[4].setX(400);
+        casillas[5].setX(480);
+        casillas[6].setX(480);
+        casillas[7].setX(400);
+        casillas[8].setX(320);
+        casillas[9].setX(220);
+        casillas[10].setX(150);
+        casillas[11].setX(50);
+        jugador.setX(casillas[0].getX());
+        jugador.setY(casillas[0].getY());
     }
 
     /**
@@ -32,30 +61,79 @@ public class GameForm extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        Tirar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Daniel\\Documents\\NetBeansProjects\\Proyecto1\\src\\main\\java\\com\\mycompany\\Assets\\img.jpg")); // NOI18N
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Daniel\\Documents\\NetBeansProjects\\Proyecto1\\src\\main\\java\\com\\mycompany\\Assets\\Dados.gif")); // NOI18N
+
+        Tirar.setText("Tirar");
+        Tirar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TirarActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Moverse");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(108, 108, 108)
+                .addGap(29, 29, 29)
                 .addComponent(jLabel1)
-                .addContainerGap(258, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(46, 46, 46)
+                        .addComponent(Tirar, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(24, 24, 24))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel3)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(67, 67, 67)
-                .addComponent(jLabel1)
-                .addContainerGap(219, Short.MAX_VALUE))
+                .addGap(54, 54, 54)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Tirar)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(45, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 436, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TirarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TirarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TirarActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.contador++;
+        jugador.setX(casillas[this.contador].getX());
+        jugador.setY(casillas[this.contador].getY());
+        repaint();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -93,6 +171,21 @@ public class GameForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Tirar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
+
+
+    public void paint(Graphics g){
+        super.paint(g);
+        g.setColor(Color.BLUE);
+        g.fillOval(50, 450, 40, 40);
+        g.setColor(Color.BLACK);
+        g.fillOval(this.jugador.getX(), this.jugador.getY(), 40, 40);
+    }
+    
+
 }
+
